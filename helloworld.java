4548a9ab870a06070979dce90d55c76cbe4004b3
Deepak1259 <deepakdamerla1259@gmail.com>
2024-09-27 17:59:15 +1200

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ class helloworld
 { 
  public static void main(String args[])
  { 
-   system.out.println("hello world");
-   system.out.println( "hii Deepak");
+   System.out.println("hello world");
+   System.out.println( "hii Deepak");
  }
 }
